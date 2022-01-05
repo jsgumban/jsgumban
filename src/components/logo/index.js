cbf0a355a90f7e4743b5205507f524ca0354ba2e
John Hel Gumban <jsgumban@up.edu.ph>
2022-01-05 23:34:32 +0800

    Initial commit

---
+++ src/components/logo/index.js
@@ -0,0 +1,25 @@
+import React,{Fragment} from 'react';
+import {Link} from "react-router-dom";
+// Logo
+import LogoDark from '../../assets/images/logo.png'
+import LogoLight from '../../assets/images/light-logo.png'
+
+const Logo = ({sideHeader}) => {
+    return (
+        <Fragment>
+            {sideHeader?(
+                <Link to={`${process.env.PUBLIC_URL + '/'}`}>
+                    jsgumban.com
+                </Link>
+            ):(
+                <div className="header-logo col-lg-2 col-6 mt-40 mb-40">
+                    <Link to={`${process.env.PUBLIC_URL + '/'}`}>
+                        jsgumban.com
+                    </Link>
+                </div>
+            )}
+        </Fragment>
+    );
+};
+
+export default Logo;
